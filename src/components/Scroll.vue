d7630db97cc86b5ea2475c527c106b156e559bd1
hao951029 <niuhao95@gamil.com>
2021-12-13 17:50:15 +0800

    错误解决：Property 'value' does not exist on type 'EventTarget'.

--- src/components/Scroll.vue
+++ src/components/Scroll.vue
@@ -9,12 +9,14 @@ defineProps<{
   modelValue: number | string,
   title: string
 }>()
+
+const handleInputChange = (event: Event) => (event.target as HTMLInputElement).value
 </script>
 
 <template>
   <div>
     <!-- attrs: https://labs.thisdot.co/blog/build-advanced-components-in-vue-3-using-usdattrs -->
     <h1>{{ title }}</h1>
-    <input v-bind="$attrs" type="range" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)">
+    <input v-bind="$attrs" type="range" :value="modelValue" @input="$emit('update:modelValue', handleInputChange($event))">
   </div>
 </template>
