6bf7c41063b21eff925ab66ade34f88716c04b59
hao951029 <niuhao95@gamil.com>
2021-12-17 14:00:01 +0800

    path.resolve

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from "path"
+import path from "path"
+
+const resolve = (dir: string) => path.resolve(__dirname, dir)
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +10,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src')
+      '@': resolve('./src')
     },
   }
 })
